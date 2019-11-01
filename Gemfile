23acb8b79ebb5db1c662a68ea996f56ced55733e
Max Nowack <max@unsou.de>
2019-11-01 16:46:23 +0100

    Implement simple server

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+gem 'sinatra'
+
+gem "trueskill", '1.0', :git => 'https://github.com/saulabs/trueskill.git'
---
+++ app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+require 'json'
+require 'bundler/setup'
+require 'saulabs/trueskill'
+
+include Saulabs::TrueSkill
+
+set :port, ENV['PORT'] || 5000
+set :dump_errors, true
+set :raise_errors, false
+
+post '/' do
+  data = JSON.parse(request.body.read)
+
+  players1 = data['team1'].map {|player| { 'userId' => player['userId'], 'rating' => Rating.new(player['mean'], player['deviation'], player['activity'])} }
+  players2 = data['team2'].map {|player| { 'userId' => player['userId'], 'rating' => Rating.new(player['mean'], player['deviation'], player['activity'])} }
+
+  team1 = players1.map { |player| player['rating'] }
+  team2 = players2.map { |player| player['rating'] }
+
+
+  # team 1 wins by 10 points against team 2
+  graph = ScoreBasedBayesianRating.new(team1 => 10.0, team2 => -10.0)
+
+  # update the Ratings
+  graph.update_skills
+
+  newData = { 'team1' => [], 'team2' => [] }
+  team1.each_index do |i|
+    newData['team1'][i] = {
+      'userId' => players1[i]['userId'],
+      'mean' => team1[i].mean,
+      'deviation' => team1[i].deviation,
+    }
+  end
+
+  team2.each_index do |i|
+    newData['team2'][i] = {
+      'userId' => players1[i]['userId'],
+      'mean' => team2[i].mean,
+      'deviation' => team2[i].deviation,
+    }
+  end
+
+  status 200
+  response.headers['Content-Type'] = 'application/json'
+  body newData.to_json
+end
